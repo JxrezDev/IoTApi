79eceafdb37550aa737a3546bcff07c618120914
matyDev <martin.juarez@itsanjuan.edu.mx>
2021-05-07 02:26:21 -0500

    Initial commit y primera Prueba de autenticacion

---
+++ config/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+func GetDBCollection() (*mongo.Collection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return nil, err
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+	collection := client.Database("GoLogin").Collection("users")
+	return collection, nil
+}
